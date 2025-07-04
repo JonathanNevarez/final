ab2ca981ad5e78f408b2579a4d4d587bb2570c61
JonathanNevarez <neva_rez00@hotmail.com>
2025-07-04 15:56:40 -0500

    final.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,15 @@
-import { Routes } from '@angular/router';​
+import { Routes } from '@angular/router';
 
-import {Home} from './pages/home/home';​
+import { Home } from './pages/home/home';
+import { Labs } from './pages/labs/labs';
 
-import {Labs} from './pages/labs/labs';​
-
-​
-export const routes: Routes = [​
-
-    {​
-
-        path: 'home',​
-
-        component: Home​
-
-    },​
-
-    {​
-
-        path: 'labs',​
-
-        component: Labs​
-
-    }​
-
- ​
-
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'labs',
+    component: Labs,
+  },
 ];
